80811a6cf0499f26c2a81dedf7bcfdb25932b33d
amandeep-singh-sandhu <sd.amansandhu@hotmail.com>
2024-01-16 20:58:04 +0530

    accordion and multiple view added

--- db/migrate/20240115104648_add_columns_to_employees.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToEmployees < ActiveRecord::Migration[7.1]
-  def change
-    add_column :employees, :job_title, :string
-    add_column :employees, :date_of_birth, :date
-    add_column :employees, :date_of_joining, :date
-    add_column :employees, :bio, :text
-  end
-end
